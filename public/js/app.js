88891726c18b5a5e1de48e56b24d56d89437e1bf
JinhuanLei <lei,jinhuan@uwlax.edu>
2018-02-18 01:40:13 -0600

    66

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
+var div1=document.getElementById("page1");
+var div2=document.getElementById("page2");
+div2.style.display='none';
 function newGame() {
+    div1.style.display='none';
+    div2.style.display='block';
     var font=$('#font').val();
     var diff=$('#diff').val();
     var wordcolor=$('#wordcolor').val();
     var guesscolor=$('#guesscolor').val();
     var forecolor=$('#forecolor').val();
 
-    $.ajax({
-        url: '/wordgame',
-        data:{"font":font,"level": diff,"wordcolor":wordcolor,"guesscolor":guesscolor,"forecolor":forecolor},
-        method: "GET",
-        success:initial
+    // $.ajax({
+    //     url: '/wordgame',
+    //     data:{"font":font,"level": diff,"wordcolor":wordcolor,"guesscolor":guesscolor,"forecolor":forecolor},
+    //     method: "GET",
+    //     success:initial
+    //
+    // });
+}
 
-    })
-    
-    
+
+
     function initial() {
         
     }
-}
