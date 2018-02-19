7613635ef9f4aabadef82641da45d0d1148310cd
JinhuanLei <lei,jinhuan@uwlax.edu>
2018-02-19 02:11:35 -0600

    2/19

--- public/module/Metadata.js
+++ public/module/Metadata.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var level = require('./level');
-
-function defaults(font,level,colors) {
-    this.font = font;
-    this.level = level;
-    this.colors=colors;
+var font= require('./font');
+var defaults=require('./defaults')
+function metadata(fonts,levels,defaults) {
+    this.fonts = fonts;
+    this.levels = levels;
+    this.defaults=defaults;
 }
+
+metadataobj=new metadata(font.getfontDb(),level.gettLevelList(),defaults.getDefaultobj());
+
+function getMetadata() {
+    return metadataobj;
+}
+
+module.exports.getMetadata = getMetadata;
